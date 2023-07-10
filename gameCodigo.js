e33fc2bc02777e0bd3f64ea35060a894a7d491cb
Leckller <7leckler@gmail.com>
2023-07-10 11:51:06 -0300

    feat: function interfaceJogo

--- gameCodigo.js
+++ gameCodigo.js
@@ -2,7 +2,6 @@ function displayGame () {
   const local = document.querySelector('#contGame');
   const divPrincipal = document.createElement('div');
   divPrincipal.id = 'options';
-  divPrincipal.className = "gameDiv"
   const newGame = document.createElement('button');
   newGame.id = 'newGameBtn';
   newGame.innerText = 'Novo Jogo';
@@ -10,11 +9,37 @@ function displayGame () {
   divPrincipal.appendChild(newGame);
 };
 
+function interfaceJogo () {
+  const divOptions = document.querySelector('#options');
+  const divTela = document.createElement('div');
+  const divInterface = document.createElement('div');
+  divTela.className = 'grow3';
+  divTela.id = 'Tela';
+  divInterface.className = 'grow2';
+  divInterface.id = 'Interface';
+  divOptions.appendChild(divTela);
+  divOptions.appendChild(divInterface);
+  const TelaLocal = document.querySelector('#Tela');
+  const InterfaceLocal = document.querySelector('#Interface');
+  const ataque = document.createElement('button');
+  const buffElementar = document.createElement('button');
+  ataque.id = 'ataque';
+  ataque.innerText = 'Ataque';
+  buffElementar.id = 'buffElementar';
+  buffElementar.innerText = 'Buff Elementar'
+  InterfaceLocal.appendChild(ataque);
+  InterfaceLocal.appendChild(buffElementar)
+}
+
 function newGameAct () {
   const btnNewGame = document.querySelector('#newGameBtn');
-  
+  btnNewGame.addEventListener('click', (eve) => {
+    eve.target.style.display = 'none';
+    interfaceJogo()
+  })
 }
 
 window.addEventListener('load', (eve) => {
   displayGame();
+  newGameAct();
 })
